551ceb5903db56df2c260673099c547170fb3ad1
manu-drk <manu.drk.06@outlook.com>
2024-05-06 21:35:32 +0200

    page logements

--- src/pages/logements/Logements.js
+++ src/pages/logements/Logements.js
@@ -1,10 +1,8 @@
-// import React from "react";
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
 import logements from "../../datas/logements.json";
-// import Tag from "../../components/tag/Tag";
 import Slideshow from '../../components/Slideshow/Slideshow';
 import Collapse from "../../components/collapse/Collapse";
 import './Logements.scss';
@@ -14,47 +12,52 @@ const Logements = () => {
   const [logementData, setLogementData] = useState(null);
 
   useEffect(() => {
-    // Récupérer les données du logement correspondant à l'ID passé dans l'URL
     const data = logements.find((item) => item.id === id);
     setLogementData(data);
   }, [logements, id]);
 
-  //   const Collapsedecription = logements.description.map((description, i) => (
-  //     return (
-  //     <ul key={i}>
-  //       <li>{description}</li>
-  //     </ul>
-  //   );
-  // ));
+  const [isDescriptionOpen, setIsDescriptionOpen] = useState(true);
+  const [isEquipementOpen, setIsEquipementOpen] = useState(false);
 
-  // const equipements = logements.equipments.map((equipment, i) => {
-  //   return (
-  //     <ul key={i}>
-  //       <li>{equipment}</li>
-  //     </ul>
-  //   );
-  // });
+  const toggleDescription = () => {
+    setIsDescriptionOpen(!isDescriptionOpen);
+  };
 
+  const toggleEquipement = () => {
+    setIsEquipementOpen(!isEquipementOpen);
+  };
 
   return (
     <div className="logementsContainer">
       <Header />
       <div className="logement-details">
         {logementData && <Slideshow pictures={logementData.pictures} />}
-        {/* Affichage du tagName */}
         {logementData && (
           <div>
             <h2 className="logement-title">{logementData.title}</h2>
             <p className="logement-tag">Tag: {tagName}</p>
             <div className="Collapse-logements">
-              <Collapse className="Collapse-description" title="Description">
-                {/* Boucle à travers les descriptions */}
-                {logementData.description.split('. ').map((desc, index) => (
-                  <p key={index}>{desc}</p>
-                ))}
+              <Collapse
+              
+                className={`Collapse-description ${isDescriptionOpen ? "open" : ""}`}
+                title="Description"
+                isOpen={isDescriptionOpen}
+                onToggle={toggleDescription}
+              >
+                {logementData.description.split('. ').map((desc, index) => {
+        console.log("After split:", desc);
+        return <p key={index}>{desc}</p>;
+    })}
+                
+
               </Collapse>
 
-              <Collapse className="Collapse-equipement" title="Équipements">
+              <Collapse
+                className={`Collapse-equipement ${isEquipementOpen ? "open" : ""}`}
+                title="Équipements"
+                isOpen={isEquipementOpen}
+                onToggle={toggleEquipement}
+              >
                 <ul>
                   {logementData.equipments.map((equipment, index) => (
                     <li key={index}>{equipment}</li>
@@ -62,7 +65,6 @@ const Logements = () => {
                 </ul>
               </Collapse>
             </div>
-            {/* Ajoutez d'autres détails du logement ici */}
           </div>
         )}
       </div>
@@ -72,40 +74,3 @@ const Logements = () => {
 };
 
 export default Logements;
-
-
-
-
-
-// const Logements = () => {
-//   // // Utilisez le hook useParams pour récupérer les paramètres d'URL
-//   // const { id } = useParams();
-
-//   // // Trouvez le logement correspondant dans le fichier JSON en fonction de l'ID
-//   // const logement = logements.find(logement => logement.id === id);
-
-//   const { id, tagName } = useParams();
-
-//   // Trouvez le logement correspondant dans le fichier JSON en fonction de l'ID et du tag
-//   const logement = logements.find(logement => logement.id === id && logement.tags.includes(tagName));
-
-//   const tagsName = logement?.tags.map((tags, i) => {
-//     return <Tag key={i} nom={tags} />;
-//   });
-
-//   return (
-//     <div className="logements">
-//       <Header />
-//       <main>
-//         <h1>{logement.title}</h1>
-//         <div>
-//           {tagsName}</div>
-//         <img src={logement.cover} alt={logement.title} className="logements_img" />
-//       </main>
-
-//       <Footer />
-//     </div>
-//   );
-// };
-
-// export default Logements;
