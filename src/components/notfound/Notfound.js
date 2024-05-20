90223c8c2071713d0b000cf4d777afd581df621d
manu-drk <manu.drk.06@outlook.com>
2024-05-20 20:27:28 +0200

    responsive page erreur 404

--- src/components/notfound/Notfound.js
+++ src/components/notfound/Notfound.js
@@ -6,18 +6,22 @@ export default function Error() {
         <>
             <div className="error_Container">
                 <p className="error_Container__Number">404</p>
-                {/* <p className="error_Container__Text">
-                    Oups! La page que vous demandez n'existe pas.
-                    Oups! La page que <span className="mobile-only"><br /></span> vous demandez n'existe pas.
-                </p> */}
-                <p className="error_Container__Text mobile-text">
+                <div className="error_Container__text_content">
+                    <p className="error_Container__Text1">
+                    Oups! La page que                     
+                </p>
+                <p className="error_Container__Text2">                    
+                    vous demandez n'existe pas.
+                </p>
+                </div>
+                {/* <p className="error_Container__Text mobile-text">
   Oups! La page que <br /> vous demandez n'existe pas.
 </p>
 <p className="error_Container__Text desktop-text">
   Oups! La page que vous demandez n'existe pas.
-</p>
+</p> */}
 
-                <NavLink to="/" className="errorContainer__Link">
+                <NavLink to="/" className="error_Container__Link">
                     Retourner sur la page d'accueil
                 </NavLink>
             </div>
